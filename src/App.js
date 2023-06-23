5f2fc425251bc7857203c5ff22b78a978712467a
Eduardo Granetto <eduardo.granetto@anfcorp.com>
2023-06-23 16:29:40 -0300

    Fly deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         data: [],
+      };
+   }
+
+   componentDidMount() {
+      fetch('https://hi-fly.fly.dev/')
+         .then((response) => response.json())
+         .then((data) => this.setState({ data: data }))
+         .catch((error) => console.log(error));
+   }
+
+   render() {
+      const { data } = this.state;
+      return (        
+        <div>Loading...</div>,
+        <h1>Code: {data.code}</h1>,
+        <h1>Hi Var: {data.hiVar}</h1>,
+        <h1>Version: {data.versionSha}</h1>
+      );
+   }
 }
 
 export default App;
